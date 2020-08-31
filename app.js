af275e43fae2c156856f71674042b30923828a67
Sheetalpujari <you@example.com>
2020-08-31 12:33:31 +0530

    Loading backend part

---
+++ app.js
@@ -0,0 +1,27 @@
+
+require('dotenv').config()
+
+const express=require("express");
+const app=express();
+const mongoose = require('mongoose');
+const bodyParser=require("body-parser");
+const cookieParser=require("cookie-parser");
+const cors=require("cors");
+
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex:true
+}).then(()=>{
+    console.log("DB CONNECTED")
+}
+);
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+const port = process.env.PORT || 8000;
+app.listen(port,()=>{
+    console.log(`app is running at ${port}`)
+});
