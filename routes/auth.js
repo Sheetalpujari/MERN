4fb6083b91264dc74bb4b953541c2b7105a98f9d
Sheetalpujari <snehalsheetal@gmail.com>
2020-09-03 23:03:03 +0530

    saving user to DB

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,13 @@
 var express=require('express')
-var router=express.Router()
+var router=express.Router();
+const { check } = require('express-validator');
+
 const {signout,signup} = require("../controllers/auth")
 router.get("/signout",signout);
 
-router.post("/signup",signup)
+router.post("/signup",[
+    check("name","name should be atleast 3 characters").isLength({min: 3}),
+    check("email","required").isEmail(),
+    check("password","password should be atleast 3 chars").isLength({min: 3})
+],signup);
 module.exports=router;
