de0a8a9685fa79d84f52e4c88308d00bda12aeaa
Sheetalpujari <snehalsheetal@gmail.com>
2020-09-03 21:03:15 +0530

    auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,21 @@
 const User=require("../models/user")
 
 
-exports.signup=(req,res)=>{
-    const user=new User(req.body)
-    user.save((err,user) => {
-    if(err){
+exports.signup=(req, res) => {
+   const user=new User(req.body);
+   user.save((err,user)=>{
+       if(err){
         return res.status(400).json({
-            err:"NOT able to save user in DB"
+           err:"not able to save"
         })
-    }
-    res.json(user);
-});
+       }
+       res.json({
+           name:user.name,
+           email:user.email,
+           id:user._id
+       
+       });
+   });
 };
 
 
