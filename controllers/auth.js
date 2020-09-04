7658b7a2830e2fe2380574be4d84221c6ab2e55b
Sheetalpujari <snehalsheetal@gmail.com>
2020-09-04 18:30:29 +0530

    auth with middlewares

--- controllers/auth.js
+++ controllers/auth.js
@@ -65,4 +65,32 @@ exports.signout = (req, res) => {
   res.json({
     message: "User signout succesfully"
   });
+
 };
+
+//protected routes
+exports.isSignedIn = expressJwt({
+  secret: process.env.SECRET,
+  userProperty:"auth"
+});
+//custom middlewares
+exports.isAuthenticated=(req,res,next)=>{
+  let checker=req.profile && req.auth && req.profile._id==req.auth._id;
+  if (!checker){
+    return res.status(403).json({
+      error:"ACCESS DENIED"
+    });
+  }
+  next();
+};
+exports.isAdmin=(req,res,next)=>{
+  if(req.profile.role==0){
+    return res.status(403).json({
+      error:"You are not admin"
+    });
+  }
+  next();
+};
+
+
+
