d0820f8885f7014e021c8bd17d5aa6b98eefbf1d
Sheetalpujari <snehalsheetal@gmail.com>
2020-09-04 18:31:14 +0530

    adding routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ var express=require('express')
 var router=express.Router();
 const { check } = require('express-validator');
 
-const {signout,signup,signin} = require("../controllers/auth");
+const {signout,signup,signin, isSignedIn} = require("../controllers/auth");
 
 
 router.post(
@@ -23,4 +23,9 @@ router.post(
 signin
 );
 router.get("/signout",signout);
+
+router.get("/testroute",isSignedIn,()=>{
+    res.json(req.auth);
+});
+
 module.exports=router;
