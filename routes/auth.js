d9de8ea75bf5bb001520baf0a1f43cf932e1bd4d
Sheetalpujari <snehalsheetal@gmail.com>
2020-09-04 13:15:48 +0530

    added routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,17 +2,25 @@ var express=require('express')
 var router=express.Router();
 const { check } = require('express-validator');
 
-const {signout,signup,signin} = require("../controllers/auth")
-router.get("/signout",signout);
+const {signout,signup,signin} = require("../controllers/auth");
 
-router.post("/signup",[
+
+router.post(
+    "/signup",
+ [
     check("name","name should be atleast 3 characters").isLength({min: 3}),
     check("email","required").isEmail(),
     check("password","password should be atleast 3 chars").isLength({min: 3})
-],signup);
-router.post("/signin",
+],
+signup
+);
+router.post(
+    "/signin",
 [
     check("email","required").isEmail(),
     check("password","password field is required").isLength({min: 3})
-],signin);
+],
+signin
+);
+router.get("/signout",signout);
 module.exports=router;
