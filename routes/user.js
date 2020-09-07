43a78419c06c6a6195ccdf5f4ed98ebe52e2efc6
Sheetalpujari <snehalsheetal@gmail.com>
2020-09-07 13:28:54 +0530

    changes in user.js

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,13 @@
-const express=require("express");
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
 
+const { getUserById, getUser, updateUser ,userPurchaselist} = require("../controllers/user");
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 
-const{ getUserById, getUser,updateUser }=require("../controllers/user");
-const {isSignedIn,isAdmin,isAuthenticated}=require("../controllers/auth");
+router.param("userId", getUserById);
 
-router.param("userId",getUserById);
-router.get("/user/:userId",isSignedIn,isAuthenticated,getUser);
-router.put("/user/:userId",isSignedIn,isAuthenticated,getUser,updateUser);
+router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
+router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
+router.get("/orders/user/:userId", isSignedIn, isAuthenticated, userPurchaselist);
 
-
-module.exports=router;
+module.exports = router;
