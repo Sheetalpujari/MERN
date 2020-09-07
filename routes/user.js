8a7362281648f0c4086b08423df48b9e667370e3
Sheetalpujari <snehalsheetal@gmail.com>
2020-09-07 15:40:03 +0530

    added delete routes

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,7 @@ const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 
 router.param("userId", getUserById);
 
+
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
 router.get("/orders/user/:userId", isSignedIn, isAuthenticated, userPurchaselist);
