c4b2f37a1f1019184c45ee7c4dbba41f501df6fe
Sheetalpujari <snehalsheetal@gmail.com>
2020-09-10 23:32:36 +0530

    updation of tshirts to database route

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getProductById, createProduct,getProduct ,photo} = require("../controllers/product");
+const { getProductById, createProduct,getProduct ,photo,updateProduct,deleteProduct} = require("../controllers/product");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
 
@@ -19,5 +19,13 @@ router.post(
 );
 
 router.get("/product/:productId",getProduct);
-router.get("/product/photo/:productId",photo); 
+router.get("/product/photo/:productId",photo);
+
+router.delete("/product/:productId/:userId",isSignedIn,isAuthenticated,isAdmin,deleteProduct);
+router.put("/product/:productId/:userId",isSignedIn,isAuthenticated,isAdmin,updateProduct);
+
+
+
+
+
 module.exports = router;
