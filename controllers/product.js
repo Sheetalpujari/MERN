c78631ac67b7fca59b0aba95b12565b513680cc3
Sheetalpujari <snehalsheetal@gmail.com>
2020-09-10 23:31:58 +0530

    updating tshirts

--- controllers/product.js
+++ controllers/product.js
@@ -66,3 +66,56 @@ exports.photo=(req,res,next)=>{
   }
   next();
 };
+
+
+exports.deleteProduct=(req,res)=>{
+   let product=req.product;
+   product.remove((err,deletedProduct)=>{
+     if(err){
+       return res.status(400).json({
+         error:"failed to del product"
+       });
+     }
+     res.json({
+       message:"deletion was succes",
+       deletedProduct
+     });
+   });
+};
+
+exports.updateProduct=(req,res)=>{
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        error: "problem with image"
+      });
+    }
+
+    let product=req.product;
+    product=_.extend(product,fields);
+    if (file.photo) {
+      if (file.photo.size > 3000000) {
+        return res.status(400).json({
+          error: "File size too big!"
+        });
+      }
+      product.photo.data = fs.readFileSync(file.photo.path);
+      product.photo.contentType = file.photo.type;
+    }
+   
+  
+
+    product.save((err, product) => {
+      if (err) {
+        res.status(400).json({
+          error: "updation failed"
+        });
+      }
+      res.json(product);
+    });
+  });
+
+};
