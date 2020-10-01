edc12c168f08ab1bed6e8306ebdb0b18b49dcbf0
Sheetalpujari <snehalsheetal@gmail.com>
2020-10-01 21:52:52 +0530

    changes in frontend

---
+++ projfrontend/src/user/Signin.js
@@ -0,0 +1,14 @@
+import React,{ useState } from "react";
+import Base from "../core/Base";
+import {Link} from "react-router-dom";
+
+const Signin = () =>{
+
+    return (
+        <Base title="signin page" description="A Page for user to signin!">
+        <h1>Sign in works!</h1>
+        </Base>
+    );
+};
+
+export default Signin;
---
+++ projfrontend/src/user/Signup.js
@@ -0,0 +1,15 @@
+import React,{ useState } from "react";
+import Base from "../core/Base";
+
+import {Link} from "react-router-dom";
+
+const Signup = () =>{
+
+    return (
+        <Base title="signup page" description="A Page for user to signup!">
+        <h1>Sign up works!</h1>
+        </Base>
+    );
+};
+
+export default Signup;
